8a9fd3d936c946cb5b9e94035d97f2e68976e339
Tanvir3744 <biplobmohammad1@gmail.com>
2024-03-17 00:24:04 +0600

    facutly created

--- dist/app/modules/student/student.constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.studentFilterableFields = exports.studentSearchableFields = exports.bloodGroup = exports.gender = void 0;
-exports.gender = ['male', 'female'];
-exports.bloodGroup = ['A+', 'A-', 'B+', 'B-', 'O+', 'O-', 'AB+', 'AB-'];
-exports.studentSearchableFields = ['email', 'íd', 'contactNo', 'name.firstName', 'name.lastName', 'name.middleName'];
-exports.studentFilterableFields = ['searchTerm', 'id', 'AcademicFaculty', 'bloodGroup', 'contactNo', 'emergencyContactNo'];
