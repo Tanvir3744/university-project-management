d232ca9117e0fd6cf6a3a9e1c45c4a24c8645651
Tanvir3744 <biplobmohammad1@gmail.com>
2024-03-20 02:42:02 +0600

    controller and routes has been created for admin and faculty

--- .history/src/app/modules/users/user.route_20240320024037.ts
+++ .history/src/app/modules/users/user.route_20240320024037.ts
@@ -5,7 +5,8 @@ import validateRequest from '../../middlewares/validateRequest';
 
 const router = express.Router();
 
-router.post("/create-student", validateRequest(userValidation.createUserZodSchema), UserController.createUser);
+router.post("/create-student", validateRequest(userValidation.createUserZodSchema), UserController.createStudent);
+router.post('/create-faculty', UserController.createFaculty)
 
 
 export const UserRoutes = router;
--- .history/src/app/modules/users/user.route_20240320024117.ts
+++ .history/src/app/modules/users/user.route_20240320024117.ts
@@ -6,6 +6,8 @@ import validateRequest from '../../middlewares/validateRequest';
 const router = express.Router();
 
 router.post("/create-student", validateRequest(userValidation.createUserZodSchema), UserController.createStudent);
+router.post('/create-faculty',validateRequest(userValidation.createFacultyZodSchema) ,UserController.createFaculty)
+router.post('/create-admin',validateRequest(userValidation.createAdminZodSchema) ,UserController.createAdmin)
 
 
 export const UserRoutes = router;
